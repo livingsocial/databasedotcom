2d47c8dfb5c61d156ee4edb0c83afbb540604fbb
campbell <pete@sumirolabs.com>
2012-04-10 09:14:22 -0400

    LS version bump

--- lib/databasedotcom/ls-version.rb
+++ lib/databasedotcom/ls-version.rb
@@ -1,4 +1,4 @@
 # This allows us to version this gem for LS but stay in sync with the main project, hence the fourth version component.
 module Databasedotcom
-  VERSION = "1.3.0.3"
+  VERSION = "1.3.0.4"
 end
